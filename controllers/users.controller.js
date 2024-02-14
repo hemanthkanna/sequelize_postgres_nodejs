9ef06dd2919d7414d2f543c6745d020d1f22cbfd
hemanthkanna <hemanthkanna20@gmail.com>
2024-02-14 15:51:42 +0530

    updateUser / users.controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -53,6 +53,33 @@ exports.getSingleUser = async(req, res) => {
     }
 }
 
+exports.updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await User.update(req.body, {
+            where : {userId : userId},
+        });
+
+        if(!user) {
+            res.status(404).json({
+                success: false,
+                message : `Cannot update user with userId ${userId}. Maybe user not found`
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                user
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message : error.message
+        })
+    }
+}
+
 exports.deleteUser = async (req, res) => {
   try {
     const userId = req.params.id;
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createUser, deleteUser, getUsers, getSingleUser } = require('../controllers/users.controller');
+const { createUser, deleteUser, getUsers, getSingleUser, updateUser } = require('../controllers/users.controller');
 const router = express.Router();
 
 router.route('/').post(createUser);
 router.route('/').get(getUsers);
 router.route('/:id').get(getSingleUser);
+router.route('/:id').put(updateUser);
 router.route('/:id').delete(deleteUser);
 
 module.exports = router;
